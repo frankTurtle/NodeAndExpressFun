59be3860b8424c9f9369d0282995508593488020
Barret J. Nobel <bear.nobel@gmail.com>
2017-12-19 15:51:50 -0500

    able to auto inject all css and js files directly into html page

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,15 @@ var gulp = require('gulp');
 
 gulp.task('inject', function () {
     var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css',
+        './public/js/*.js'], { read: false });
+
+    var injectOptions = {
+        ignorePath: 'public'
+    };
+
     var options = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
@@ -10,5 +19,6 @@ gulp.task('inject', function () {
 
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 });
